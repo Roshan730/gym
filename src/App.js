ce825e175716bb005f2113cd479e7eaab373dba8
lucifer098a <nk5141030@gmail.com>
2022-07-20 00:12:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Join from "./components/Join/Join";
+import Plans from "./components/Plans/Plans";
+import Program from "./components/Program/Program";
+import Reason from "./components/Reason/Reason";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <Program />
+      <Reason />
+      <Plans />
+      <Testimonials />
+      <Join />
     </div>
   );
 }
